90bc40dc1860cf64ac1f4e0db63a3394f1b22298
marcelamorena <marcelamorena904@gmail.com>
2026-05-25 21:46:38 -0300

    Adiciona tela de cadastro

--- cafefinace_ready/frontend/cafefinance/src/app/app.routes.ts
+++ cafefinace_ready/frontend/cafefinance/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
+import { Cadastro } from './components/cadastro/cadastro';
 import { Home } from './components/home/home';
 import { Login } from './components/login/login';
 
 export const routes: Routes = [
   { path: '', component: Login },
+  { path: 'cadastro', component: Cadastro },
   { path: 'home', component: Home },
   { path: '**', redirectTo: '' },
 ];
