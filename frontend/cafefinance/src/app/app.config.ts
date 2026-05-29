6a76c58ab32074130b194dd2d857878ff80edd2a
GABRIEL BARBOSA DOS SANTOS <gabriel.santos@gabaritoeducacao.com>
2026-05-29 09:24:25 -0300

    Corrige estrutura do projeto

--- frontend/cafefinance/src/app/app.config.ts
+++ frontend/cafefinance/src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- frontend/cafefinance/src/app/app.routes.ts
+++ frontend/cafefinance/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Cadastro } from './components/cadastro/cadastro';
 import { Home } from './components/home/home';
 import { Login } from './components/login/login';
+import { Perfil } from './components/perfil/perfil';
 
 export const routes: Routes = [
   { path: '', component: Login },
   { path: 'cadastro', component: Cadastro },
+  { path: 'perfil', component: Perfil },
   { path: 'home', component: Home },
   { path: '**', redirectTo: '' },
 ];
